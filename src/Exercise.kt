03c13d7acb8f0d9ef4c1f00e16117843e125190d
Simone Li Causi <simonelicausi96@gmail.com>
2023-07-07 17:34:34 +0200

    Initial commit

---
+++ src/Exercise.kt
@@ -0,0 +1,29 @@
+// Scrivi un ciclo che stampa numeri da 1 a 100, aumentando di 2 (quindi 1, 3, 5...).
+// Utilizzare due diverse funzioni di stampa per valori inferiori a 50 e valori superiori a 50.
+
+
+fun main() {
+
+    for (i in 1..100 step 2) {
+        println("${i}")
+        if (i < 50) {
+            printSmallNumbers(i)
+        } else {
+            printBigNumbers(i)
+        }
+
+    }
+}
+
+fun printSmallNumbers(smallNumber: Int) {
+    println("Small numbers: ${smallNumber}")
+}
+
+fun printBigNumbers(BigNumber: Int) {
+    println("Big numbers: ${BigNumber}")
+}
+
+
+
+
+
